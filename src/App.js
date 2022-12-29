be9da7f25fdb4770a9771e33826a01d9461fcce8
darikmohammed <r.m.darik@gmail.com>
2022-12-29 15:10:31 +0300

    Init file with react3fiber basic mesh box

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <color attach="background" args={['#695b5b']} />
+      <OrbitControls makeDefault />
+
+      <mesh>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
+    </>
   );
 }
 
